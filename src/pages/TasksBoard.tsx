4d0d2f79e4b8b1ae9c36f68008df9ffc6f95f47f
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-10-30 20:24:37 +0545

    fix: style

--- src/pages/TasksBoard.tsx
+++ src/pages/TasksBoard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "../components/Header";
+import TasksList from "../components/TasksList";
 
 function TasksBoard() {
   const [searchValue, setSearchValue] = useState<string>("");
@@ -8,8 +9,9 @@ function TasksBoard() {
     setSearchValue(value);
   };
   return (
-    <div className="w-full">
+    <div className="w-full gap-10 flex flex-col justify-center items-center">
       <Header onSearch={handleSearch} />
+      <TasksList />
     </div>
   );
 }
